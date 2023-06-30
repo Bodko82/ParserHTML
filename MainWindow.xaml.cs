0bebbbb57c52b1fcc15969c38761333c6bd44ec5
Bodko82 <Bodko82@gmail.com>
2023-06-30 11:43:00 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using ClosedXML.Excel;
+using Microsoft.Win32;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ParserHTML
+{
+    public partial class MainWindow : Window
+    {
+        private ProductParser _parser;
+        public ObservableCollection<Product> Products { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _parser = new ProductParser();
+            Products = new ObservableCollection<Product>();
+            DataContext = this;
+        }
+
+        private async void ParseButton_Click(object sender, RoutedEventArgs e)
+        {
+            var parsedProducts = await _parser.ParseProducts();
+            Products = new ObservableCollection<Product>(parsedProducts);
+
+            if (parsedProducts.Count > 0)
+            {
+                productListView.ItemsSource = Products;
+                exportButton.IsEnabled = true;
+                exportDirectButton.IsEnabled = true;
+            }
+        }
+        private async void ExportDirectButton_Click(object sender, RoutedEventArgs e)
+        {
+            var saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Excel Files|*.xlsx";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string filePath = saveFileDialog.FileName;
+
+                var parsedProducts = await _parser.ParseProducts();
+                ExportToExcel(filePath, parsedProducts);
+            }
+        }
+
+        private void ScrollViewer_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            ScrollViewer scrollViewer = (ScrollViewer)sender;
+            scrollViewer.ScrollToVerticalOffset(scrollViewer.VerticalOffset - e.Delta);
+            e.Handled = true;
+        }
+
+        private void ExportButton_Click(object sender, RoutedEventArgs e)
+        {
+            var saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Excel Files|*.xlsx";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string filePath = saveFileDialog.FileName;
+                ExportToExcel(filePath, Products.ToList());
+            }
+        }
+        private void ExportToExcel(string filePath, IEnumerable<Product> productList)
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Products");
+                var gridView = productListView.View as GridView;
+                if (gridView != null)
+                {
+                    for (int i = 0; i < gridView.Columns.Count; i++)
+                    {
+                        GridViewColumn column = gridView.Columns[i];
+                        worksheet.Cell(1, i + 1).Value = column.Header.ToString();
+                    }
+                    var rowIndex = 2;
+                    foreach (var product in productList)
+                    {
+                        for (int j = 0; j < gridView.Columns.Count; j++)
+                        {
+                            GridViewColumn column = gridView.Columns[j];
+                            string property = ((Binding)column.DisplayMemberBinding).Path.Path;
+                            object value = product.GetType().GetProperty(property).GetValue(product);
+                            worksheet.Cell(rowIndex, j + 1).Value = value?.ToString();
+                        }
+                        rowIndex++;
+                    }
+                }
+
+                workbook.SaveAs(filePath);
+            }
+        }
+
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ParserHTML
+{
+    public class Product
+    {
+        [JsonPropertyName("productVendor")]
+        public string Vendor { get; set; }
+
+        [JsonPropertyName("productName")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("productPartNumber")]
+        public string Number { get; set; }
+
+        [JsonPropertyName("productDetails")]
+        public string Details { get; set; }
+
+        [JsonPropertyName("productDiscountPrice")]
+        public string Price { get; set; }
+
+        [JsonPropertyName("statusMessage")]
+        public string Status { get; set; }
+
+        [JsonPropertyName("longURL")]
+        public string Url { get; set; }
+    }
+
+    public class ProductParser
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProductParser()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<ObservableCollection<Product>> ParseProducts()
+        {
+            var baseUrl = "https://www.swansonvitamins.com/ncat1/Vitamins+and+Supplements/ncat2/Multivitamins/ncat3/Multivitamins+with+Iron/q";
+            var pageNumber = 1;
+            var products = new ObservableCollection<Product>();
+
+            while (true)
+            {
+                var url = $"{baseUrl}?page={pageNumber}";
+                    var html = await _httpClient.GetStringAsync(url);
+
+                    var pattern = @"adobeRecords"":(.+),""topProduct";
+                    var matches = Regex.Matches(html, pattern);
+
+                    if (matches.Count > 0)
+                    {
+                        var json = matches[0].Groups[1].Value;
+                        var productList = JsonSerializer.Deserialize<List<Product>>(json);
+                    if (productList.Count <=0) { break; }
+                        productList.ForEach(p =>
+                        {
+                            products.Add(p);
+                        });
+                    }
+                    pageNumber++;
+            }
+
+            return products;
+        }
+
+        private async Task<bool> UrlExists(string url)
+        {
+            try
+            {
+                var request = new HttpRequestMessage(HttpMethod.Head, url);
+                var response = await _httpClient.SendAsync(request);
+                return response.IsSuccessStatusCode;
+            }
+            catch (HttpRequestException)
+            {
+                return false;
+            }
+        }
+    }
+}
